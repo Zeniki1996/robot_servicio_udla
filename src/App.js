c17ca3e14b0240105e9d0c0789dbb2722bf5cb74
Nestor Lozada <martinlozada1@gmail.com>
2024-05-14 11:39:47 -0500

    adding STT (speach to text)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import './App.css'; // Importa tus estilos CSS aquí
 
 const SpeechToTextComponent = () => {
   const [text, setText] = useState('');
@@ -21,30 +22,29 @@ const SpeechToTextComponent = () => {
   };
 
   return (
-    <div>
-      <textarea
-        rows="4"
-        cols="50"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleListen}>
-        <img src="microphone-icon.png" alt="Microphone" />
-      </button>
-      <button onClick={handleStop}>Detener</button>
+    <div className="centered">
+      <div className="pixel-art-container">
+        <div className="pixel-art">
+          <div className="smiley-face">😊</div>
+          <textarea
+            rows="4"
+            cols="50"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={handleListen}>
+            <img src="microphone-icon.png" alt="Microphone" />
+          </button>
+          <button onClick={handleStop}>Detener</button>
+        </div>
+      </div>
     </div>
   );
 };
 
-const SmileyFaceComponent = () => {
-  // Aquí iría tu componente de la carita sonriendo
-  return <div>😊</div>;
-};
-
 const App = () => {
   return (
-    <div>
-      <SmileyFaceComponent />
+    <div className="background">
       <SpeechToTextComponent />
     </div>
   );
