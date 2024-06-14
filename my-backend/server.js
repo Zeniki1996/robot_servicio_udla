ecbb79df8d2d47912508e353ed99a2c0ebb6eeee
Nestor Lozada <martinlozada1@gmail.com>
2024-06-14 14:11:03 -0500

    proyecto animaciones + conexion a azure

---
+++ my-backend/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+// Configura el middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Ruta para manejar la solicitud a Azure OpenAI
+app.post('/api/openai', async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await axios.post(
+      'https://nonprodchatrobotopenai.openai.azure.com/openai/deployments/implmentacionchatservicioUDLA/chat/completions?api-version=2024-02-01',
+      {
+        data_sources: [
+          {
+            type: "azure_search",
+            parameters: {
+              endpoint: "https://nonprodchatrobotaisearch.search.windows.net",
+              index_name: "contenedorudlaservicebot",
+              semantic_configuration: "default",
+              query_type: "simple",
+              fields_mapping: {},
+              in_scope: true,
+              role_information: "Te llamas ULI, eres un asistente virtual para la Universidad de las Americas (UDLA). Si te saludan debes responder con otro saludo, dando la bienvenida y explicando tu nombre y función. Es muy importante que si te realizan preguntas no relacionadas a la universidad simplemente respondas: 'Lo siento, no estoy seguro'. Responderás siempre de forma amigable y tratando de ayudar. Tratarás de responder en español a menos que las preguntas sean en otros idiomas.",
+              filter: null,
+              strictness: 3,
+              top_n_documents: 5,
+              authentication: {
+                type: "api_key",
+                key: ""
+              },
+              key: ""
+            }
+          }
+        ],
+        messages: [
+          {
+            role: "system",
+            content: "Te llamas ULI, eres un asistente virtual para la Universidad de las Americas (UDLA). Si te saludan debes responder con otro saludo, dando la bienvenida y explicando tu nombre y función. Es muy importante que si te realizan preguntas no relacionadas a la universidad simplemente respondas: 'Lo siento, no estoy seguro'. Responderás siempre de forma amigable y tratando de ayudar. Tratarás de responder en español a menos que las preguntas sean en otros idiomas. Y con cada respuesta escribe una palabra de emoción que detectes en la conversación"
+          },
+          {
+            role: "user",
+            content: prompt
+          }
+        ],
+        temperature: 0,
+        top_p: 1,
+        max_tokens: 800
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': '1d896159bce74b84a433836f6d660d8c'
+        }
+      }
+    );
+
+    res.json({ text: response.data.choices[0].message.content });
+  } catch (error) {
+    console.error('Error al llamar a Azure OpenAI:', error);
+    res.status(500).send('Error al llamar a Azure OpenAI');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,86 +1,153 @@
-import React, { useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import TelegramBot from 'node-telegram-bot-api'; // Importa la biblioteca
-import './App.css';
+import React, { useState, useEffect } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import "./App.css";
 
-const botToken = '7096839273:AAEGLfilLM7OT-96oFGNT6oYT5DtvC97fdI'; // Reemplaza con tu propio token
-const chatId = '7096839273'; // Reemplaza con el ID de chat al que quieres enviar mensajes
-const bot = new TelegramBot(botToken);
-
-// Componentes para la cara
-const Eyes = () => {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ width: '100px', height: '100px', backgroundColor: 'black', borderRadius: '50%', marginRight: '10px', marginBottom:'10px' }}></div>
-      <div style={{ width: '100px', height: '100px', backgroundColor: 'black', borderRadius: '50%' }}></div>
-    </div>
-  );
-};
-
-const Mouth = () => {
-  return (
-    <div style={{ width: '150px', height: '50px', backgroundColor: 'red', margin: '0 auto', marginBottom:'10px' }}></div>
-  );
+// Imágenes utilizadas en el estado de la aplicación
+const images = {
+  abierto: require("./images/abierto.png"),
+  feliz: require("./images/feliz.png"),
+  cansado: require("./images/cansado.png"),
+  enojado: require("./images/enojado.png"),
+  guino: require("./images/guiño.png"),
+  pensando: require("./images/pensando.png"),
+  triste: require("./images/triste.png"),
 };
 
 const SpeechToTextComponent = () => {
-  const [text, setText] = useState('');
-  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [text, setText] = useState(""); // Estado para almacenar el texto reconocido
+  const [status, setStatus] = useState("En espera"); // Estado para manejar el estado del reconocimiento de voz
+  const [image, setImage] = useState(images.abierto); // Estado para manejar la imagen actual
+  const { transcript, resetTranscript } = useSpeechRecognition(); // Hook para el reconocimiento de voz
 
+  // Efecto para manejar el cambio de imagen en base al estado
+  useEffect(() => {
+    let interval;
+    if (status === "En espera") {
+      interval = setInterval(() => {
+        setImage(prevImage => (prevImage === images.feliz ? images.abierto : images.feliz));
+      }, 2500); // 2s feliz, 0.5s abierto
+    } else if (status === "Escuchando" || status === "Hablando") {
+      interval = setInterval(() => {
+        setImage(prevImage => (prevImage === images.abierto ? images.feliz : images.abierto));
+      }, 2500); // 2s abierto, 0.5s feliz
+    }
+    return () => clearInterval(interval); // Limpieza del intervalo cuando el componente se desmonta o el estado cambia
+  }, [status]);
+
+  // Función para iniciar el reconocimiento de voz
   const handleListen = () => {
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
       alert("Tu navegador no soporta reconocimiento de voz");
       return;
     }
-
-    SpeechRecognition.startListening();
-    sendMessageToTelegram("OK"); 
-
+    // Iniciar reconocimiento de voz en español
+    SpeechRecognition.startListening({ language: "es-ES" });
+    setStatus("Escuchando");
   };
 
+  // Función para detener el reconocimiento de voz
   const handleStop = () => {
     SpeechRecognition.stopListening();
     setText(transcript);
     resetTranscript();
-  };
 
-  const handleSpeak = () => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(utterance);
-  };
+    if (transcript.trim()) {
+      setStatus("Pensando");
+      setImage(images.pensando);
 
-  // Función para enviar mensaje a Telegram
-  const sendMessageToTelegram = (message) => {
-    bot.sendMessage(chatId, message)
-      .then(() => {
-        console.log('Mensaje enviado a Telegram con éxito');
+      // Enviar el texto reconocido al backend
+      fetch("http://localhost:3001/api/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ prompt: transcript })
       })
-      .catch((error) => {
-        console.error('Error al enviar el mensaje a Telegram:', error);
-      });
+        .then(response => response.json())
+        .then(data => {
+          const responseText = data.text; // Suponiendo que el backend devuelve { text: "respuesta del bot" }
+          setStatus("Hablando");
+          handleSpeak(responseText);
+        })
+        .catch(error => {
+          console.error("Error al llamar a la API:", error);
+          setStatus("En espera");
+          setImage(images.abierto);
+        });
+    } else {
+      alert("No se detectó contenido de la voz");
+      setStatus("En espera");
+      setImage(images.abierto);
+    }
   };
+
+  // Función para convertir texto en voz
+  const handleSpeak = (textToSpeak) => {
+    if (textToSpeak.trim()) {
+      setStatus("Hablando");
+      const utterance = new SpeechSynthesisUtterance(textToSpeak);
+      utterance.lang = "es-ES"; // Establecer el idioma a español
+      utterance.onend = () => {
+        setImage(images.guino);
+        setTimeout(() => {
+          setStatus("En espera");
+          setImage(images.abierto);
+        }, 2000); // Cambiar la imagen a "guiño" por 2 segundos cuando termina de hablar
+      };
+      window.speechSynthesis.speak(utterance);
+    } else {
+      alert("No hay contenido en el cuadro de texto");
+    }
+  };
+
+  // Función para manejar la detección de la frase desencadenadora
+  const handleWakeWord = () => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Tu navegador no soporta reconocimiento de voz");
+      return;
+    }
+    SpeechRecognition.startListening({ continuous: true, language: "es-ES" });
+
+    SpeechRecognition.onresult = (event) => {
+      const transcript = event.results[event.results.length - 1][0].transcript.trim();
+      if (transcript.toLowerCase().includes("hola uli")) {
+        SpeechRecognition.stopListening();
+        setStatus("Escuchando");
+        handleListen();
+
+        // Detener automáticamente después de 5 segundos
+        setTimeout(() => {
+          handleStop();
+        }, 5000);
+      }
+    };
+  };
+
+  useEffect(() => {
+    handleWakeWord();
+  }, []);
 
   return (
     <div className="centered">
       <div className="pixel-art-container">
+        <img src={image} alt="Estado actual" className="state-image" />
         <div className="pixel-art">
-          <Eyes />
-          <Mouth />
           <textarea
             rows="4"
             cols="50"
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          <div className="button-container">
             <button onClick={handleListen}>
               <img src="microphone-icon.png" alt="Microphone" />
             </button>
             <button onClick={handleStop}>Detener</button>
-            <button onClick={handleSpeak}>Hablar</button>
+            <button onClick={() => handleSpeak(text)}>Hablar</button>
           </div>
         </div>
       </div>
+      <span style={{ color: "white" }}>{status}</span>
     </div>
   );
 };
